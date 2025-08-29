a7da681b2b90f6ed00092a13b970b61d3001cfc9
Ewerton Lourenço <ewerton_lourenco@outlook.com>
2025-08-29 02:56:44 -0300

    test(unit-domain): add Order tests and test data following existing folder layout

---
+++ tests/Ambev.DeveloperEvaluation.Unit/Domain/Entities/TestData/OrderTestData.cs
@@ -0,0 +1,13 @@
+﻿namespace Ambev.DeveloperEvaluation.Unit.Domain.Entities.TestData;
+
+using Ambev.DeveloperEvaluation.Domain.Entities;
+
+public static class OrderTestData
+{
+    public static Order NewOrderWithItem(string desc, decimal unit, int qty)
+    {
+        var o = new Order();
+        o.AddItem(new OrderItem { Description = desc, UnitPrice = unit, Quantity = qty });
+        return o;
+    }
+}
